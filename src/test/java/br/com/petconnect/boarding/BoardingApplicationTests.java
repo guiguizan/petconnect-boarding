719085b17419a05a837d026b627133179677b4c7
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-08-22 20:42:03 -0300

    chore: created first config

---
+++ src/test/java/br/com/petconnect/boarding/BoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petconnect.boarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
