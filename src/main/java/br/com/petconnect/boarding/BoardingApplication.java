0e46f8258b03e7b3c036a5983dac01a9b524a1a0
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-08-22 21:16:29 -0300

    chore: remove trim

--- src/main/java/br/com/petconnect/boarding/BoardingApplication.java
+++ src/main/java/br/com/petconnect/boarding/BoardingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardingApplication.class, args);
 	}
