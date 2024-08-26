b917649d01f883784aab72bc7aee4a83370aa232
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-08-25 21:34:56 -0300

    chore: add DockerFile and buildspec.yaml

---
+++ src/main/java/br/com/petconnect/boarding/controller/UserController.java
@@ -0,0 +1,17 @@
+package br.com.petconnect.boarding.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String teste(){
+        return "Hello PET";
+    }
+}
