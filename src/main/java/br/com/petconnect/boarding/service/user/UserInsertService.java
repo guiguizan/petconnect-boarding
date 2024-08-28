28ba5047a0555ce97032ff686aab9632bbcb4639
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-08-28 14:14:14 -0300

    feat: added method for tratament of password crypto

--- src/main/java/br/com/petconnect/boarding/service/user/UserInsertService.java
+++ src/main/java/br/com/petconnect/boarding/service/user/UserInsertService.java
@@ -26,6 +26,7 @@ public class UserInsertService {
 
 
         final User user = UserMapper.INSTANCE.toUser(insertUserDto);
+
         System.out.println(user);
         user.setCreatedAt(currentTime);
         user.setUpdatedAt(currentTime);
