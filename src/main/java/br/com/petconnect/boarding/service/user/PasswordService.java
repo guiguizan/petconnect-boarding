fbaba033f84cc408578795b7142129fc520e5913
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-08-28 13:43:57 -0300

    feat: added method for tratament of password crypto

--- src/main/java/br/com/petconnect/boarding/service/user/PasswordService.java
+++ src/main/java/br/com/petconnect/boarding/service/user/PasswordService.java
@@ -13,23 +13,12 @@ public class PasswordService {
 
 
 
-    /**
-     * Gera um hash para a senha fornecida.
-     *
-     * @param rawPassword a senha original
-     * @return a senha criptografada
-     */
+
     public String encryptPassword(String rawPassword) {
         return passwordEncoder.encode(rawPassword);
     }
 
-    /**
-     * Verifica se a senha fornecida corresponde ao hash armazenado.
-     *
-     * @param rawPassword a senha original
-     * @param encryptedPassword o hash da senha armazenado
-     * @return true se a senha corresponder, false caso contrário
-     */
+
     public boolean verifyPassword(String rawPassword, String encryptedPassword) {
         return passwordEncoder.matches(rawPassword, encryptedPassword);
     }
