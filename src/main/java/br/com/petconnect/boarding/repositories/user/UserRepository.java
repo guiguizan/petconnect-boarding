49cf23cb8b635a9886b46ecd588b409f0655867c
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-08-31 16:33:07 -0300

    feat: added reset password

--- src/main/java/br/com/petconnect/boarding/repositories/user/UserRepository.java
+++ src/main/java/br/com/petconnect/boarding/repositories/user/UserRepository.java
@@ -2,6 +2,8 @@ package br.com.petconnect.boarding.repositories.user;
 
 import br.com.petconnect.boarding.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +13,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByCpf(String cpf);
     Boolean existsByEmail(String email);
-
-    Optional<User> findByEmail(String Email);
+    @Query("SELECT u FROM br.com.petconnect.boarding.domain.User u WHERE LOWER(u.email) = LOWER(:email)")
+    Optional<User> findByEmail(@Param("email") String email);
 }
