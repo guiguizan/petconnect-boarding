ca8b4ef77f0b63fe67ce6273325dc76244c806e7
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-09-04 13:38:20 -0300

    fix: add constructor for DTO

--- src/main/java/br/com/petconnect/boarding/dto/request/InsertUserContactsDto.java
+++ src/main/java/br/com/petconnect/boarding/dto/request/InsertUserContactsDto.java
@@ -3,11 +3,15 @@ package br.com.petconnect.boarding.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class InsertUserContactsDto {
     @NotBlank(message = "O tipo de contato é obrigatório e não pode estar em branco")
     private String type;
