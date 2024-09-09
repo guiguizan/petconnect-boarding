3b8534507626e082c17064c01c70e6a2e06f09e7
Guilherme Zanchettin <guiguizanchettin@hotmail.com>
2024-09-09 14:13:14 -0300

    feat: add preAuthorize

--- src/main/java/br/com/petconnect/boarding/controller/AdministratorController.java
+++ src/main/java/br/com/petconnect/boarding/controller/AdministratorController.java
@@ -2,22 +2,40 @@ package br.com.petconnect.boarding.controller;
 
 import br.com.petconnect.boarding.dto.request.UpdateRoleRequestDto;
 import br.com.petconnect.boarding.dto.response.DefaultMessageDto;
+import br.com.petconnect.boarding.dto.response.UserResponseDto;
 import br.com.petconnect.boarding.service.administrator.AdministratorService;
+import br.com.petconnect.boarding.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("api/v1/administrator")
 @RequiredArgsConstructor
 public class AdministratorController {
     private final AdministratorService administratorService;
+    private final UserService userService;
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('USER_ADMIN')")
     public DefaultMessageDto updateRole(@RequestBody UpdateRoleRequestDto updateRoleRequestDto){
         return administratorService.updateEmployeeRole(updateRoleRequestDto);
     }
+
+
+    @GetMapping("/list-user")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('USER_ADMIN')")
+    public Page<UserResponseDto> listUser(@PageableDefault(sort = "idUser") Pageable pageable){
+        return userService.getUsers(pageable);
+    }
+
 
 }
